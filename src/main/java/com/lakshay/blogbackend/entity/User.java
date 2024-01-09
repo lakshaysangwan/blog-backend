1182b28b328945089a5c9016961ae343074b6738
Lakshay Sangwan <lakshay.ib.hisar@gmail.com>
2024-01-09 18:58:33 +0530

    RequriedArgsConstructor added.

--- src/main/java/com/lakshay/blogbackend/entity/User.java
+++ src/main/java/com/lakshay/blogbackend/entity/User.java
@@ -1,32 +1,40 @@
 package com.lakshay.blogbackend.entity;
 
-import com.lakshay.blogbackend.error.custom_error.sign_up.SignUpException;
-import com.lakshay.blogbackend.error.custom_error.sign_up.enums.SignUpExceptionCodes;
+import com.lakshay.blogbackend.exception.custom_exception.user_exception.UserException;
+import com.lakshay.blogbackend.exception.custom_exception.user_exception.UserExceptionCodes;
 import com.lakshay.blogbackend.utilities.Utilities;
 import jakarta.validation.constraints.Email;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
+
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Document
 public class User {
     @Id
-    private String id;
-    private String firstName;
-    private String middleName;
-    private String lastName;
-    private String username;
+    private final String id;
+    private final String firstName;
+    private final String middleName;
+    private final String lastName;
+    private final String username;
     @Email(message = "Invalid email format")
-    private String email;
-    private String password;
+    private final String email;
+    private final String password;
+    @CreatedDate
+    private Date createdAt;
+    @LastModifiedDate
+    private Date updatedAt;
 
     public static User validate(String id, String firstname, String middleName, String lastname, String username, String email, String password) {
         if (Utilities.isValidEmail(email))
             return new User(id, firstname, middleName, lastname, username, email, Utilities.hashPassword(password));
         else
-            throw new SignUpException(SignUpExceptionCodes.INVALID_EMAIL_ID.getCode(), "Email id can either be null or a valid email");
+            throw new UserException(UserExceptionCodes.INVALID_EMAIL_ID.getCode(), "Email id can either be null or a valid email");
     }
 }
